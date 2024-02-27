9e4e17072b1d553922e612c7c5e3552c57d32566
rlaeksl0124 <75132079+rlaeksl0124@users.noreply.github.com>
2024-02-27 22:49:49 +0900

    YoilTeller req/res

--- src/main/java/com/example/firstSpring/YoilTeller.java
+++ src/main/java/com/example/firstSpring/YoilTeller.java
@@ -1,12 +1,20 @@
 package com.example.firstSpring;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
 import java.util.Calendar;
 
+@Controller
 public class YoilTeller {
-    public static void main(String[] args) {
-        String year = args[0];
-        String month = args[1];
-        String day = args[2];
+    @RequestMapping("/yoilTeller") // http://localhost/yoilTeller?year=2024&month=02&day=27
+    public void main(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String year = request.getParameter("year"); // year=2024
+        String month = request.getParameter("month"); // month=02
+        String day = request.getParameter("day"); // day=27
         
         int yyyy = Integer.parseInt(year);
         int mm = Integer.parseInt(month);
@@ -18,7 +26,11 @@ public class YoilTeller {
         int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
         char yoil = " 일월화수목금토".charAt(dayOfWeek);
 
-        System.out.println(year+"년 "+month+"월 "+day+"일 ");
-        System.out.println(yoil+"요일입니다");
+        // 출력
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = response.getWriter(); // response 객체에서 브라우저로의 출력 스트림을 얻는다
+        out.println(year+"년 "+month+"월 "+day+"일 ");
+        out.println(yoil+"요일입니다");
     }
 }
